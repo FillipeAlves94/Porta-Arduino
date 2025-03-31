36cf1bc836d96a1571e6587ccd37cf995ebb90d3
ValterBeff <valterbeff.2001@gmail.com>
2025-03-30 23:32:45 -0300

    SensorDistacia Finalizado

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,20 +2,21 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
-#define SERVO_PIN 7   // Pino que controla o servo motor
-#define TRIG_PIN A3   // Pino TRIG conectado ao pino analogico A3
-#define ECHO_PIN A4   // Pino ECHO conectado ao pino analogico A4
+#define TRIG_PIN A3   // Pino TRIG conectado ao A3
+#define ECHO_PIN A4   // Pino ECHO conectado ao A4
+#define SERVO_PIN 7   // Pino do servo motor
 
-#define NOTE_1  350  // Definindo a nota musical (em Hz)
-#define NOTE_2  200  // Definindo a nota musical (em Hz)
-int BUZZER_PIN = 13;  // Buzzer conectado ao pino 13
+#define NOTE_1  350  // Nota de som para acesso
+#define NOTE_2  200  // Nota de som para erro
+int BUZZER_PIN = 13;  // Pino do buzzer
+
+long tempoOpen = 0;
+long timeDistance10cm = 0;
+bool statesOpen = false;
 
 Servo portaServo;
-
-// Configuração do LCD (modo paralelo)
 LiquidCrystal lcd(12, 11, 10, 9, 8, 6);
 
-// Configuração do teclado matricial
 const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS] = {
@@ -28,15 +29,17 @@ byte rowPins[ROWS] = {5, 4, 3, 2};
 byte colPins[COLS] = {A2, A1, A0};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-
 String senhaCorreta = "0000";
 String senhaDigitada = "";
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
   portaServo.attach(SERVO_PIN);
-  portaServo.write(0);  // Porta começa fechada (posição 0°)
+  portaServo.write(0);
 
   lcd.begin(16, 2);
   lcd.print("Digite a senha:");
@@ -44,19 +47,15 @@ void setup() {
 
 void loop() {
   char key = keypad.getKey();
-
   if (key) {
-    if (key == '#') {  // Confirmar senha
+    if (key == '#') {  // Confirmação da senha
       if (senhaDigitada == senhaCorreta) {
         lcd.clear();
         lcd.print("Acesso Liberado");
         tocarNotaRepetida();
-        portaServo.write(90); // Abre a porta (posição 90°)
-        delay(2000);           // Aguarda 2 segundos
-        portaServo.write(0);  // Fecha a porta (posição 0°)
-
-        lcd.clear();
-        lcd.print("Digite a senha:");
+        portaServo.write(90);
+        statesOpen = true;
+        tempoOpen = millis();
       } else {
         lcd.clear();
         lcd.print("Senha Errada!");
@@ -65,34 +64,96 @@ void loop() {
         lcd.clear();
         lcd.print("Digite a senha:");
       }
-      senhaDigitada = "";  // Limpa a senha digitada
+      senhaDigitada = "";
     } else if (key == '*') {
-      senhaDigitada = "";  // Apagar senha
+      senhaDigitada = "";
       lcd.clear();
       lcd.print("Digite a senha:");
     } else {
       senhaDigitada += key;
       lcd.setCursor(senhaDigitada.length() - 1, 1);
-      lcd.print('*');  // Exibe '*' no lugar dos números
+      lcd.print('*');
+    }
+  }
+
+  float distanciaCM = medirDistancia();
+  controlarPorta(distanciaCM);
+}
+
+float medirDistancia() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duracao = pulseIn(ECHO_PIN, HIGH);
+  return duracao * 0.0344 / 2;
+}
+
+void controlarPorta(float distanciaCM) {
+  if (distanciaCM <= 10) {
+    if (!statesOpen) {
+      timeDistance10cm = millis();
+    }
+
+    if (millis() - timeDistance10cm >= 5000) {
+      if (statesOpen) {
+        lcd.clear();
+        lcd.print("Porta Fechada!");
+        tocarNotaFechado();
+        portaServo.write(0);
+        statesOpen = false;
+        delay(1500);
+        lcd.clear();
+        lcd.print("Digite a senha:");
+      }
+    }
+  } else {
+    timeDistance10cm = 0;
+    if (!statesOpen) {
+      portaServo.write(90);
+      statesOpen = true;
+      tempoOpen = millis();
+    }
+    if (millis() - tempoOpen >= 10000) {
+      tocarAlerta();
     }
   }
 }
 
+void tocarAlerta() {
+  tone(BUZZER_PIN, NOTE_1);
+  lcd.clear();
+  lcd.print("ALERTA - PORTA ABERTA");
+  delay(150);
+  noTone(BUZZER_PIN);
+  delay(100);
+  lcd.clear();
+  lcd.print("Digite a senha:");
+}
 
 void tocarNotaRepetida() {
   for (int i = 0; i < 3; i++) {
-    tone(BUZZER_PIN, NOTE_1);  // Emite a nota C4
-    delay(150);                  // Aguarda 150 ms (duração curta)
-    noTone(BUZZER_PIN);         // Desliga o som da nota
-    delay(100);                  // Pequeno intervalo entre as repetições
+    tone(BUZZER_PIN, NOTE_1);
+    delay(150);
+    noTone(BUZZER_PIN);
+    delay(100);
   }
+}
+
+void tocarNotaFechado(){
+    tone(BUZZER_PIN, NOTE_2);
+    delay(150);
+    noTone(BUZZER_PIN);
+    delay(100);
 }
 
 void tocarNotaRepetidaErrada() {
   for (int i = 0; i < 2; i++) {
-    tone(BUZZER_PIN, NOTE_2);  // Emite a nota C4
-    delay(150);                // Aguarda 200 ms (duração curta)
-    noTone(BUZZER_PIN);         // Desliga o som da nota
-    delay(100);                // Pequeno intervalo entre as repetições
+    tone(BUZZER_PIN, NOTE_2);
+    delay(150);
+    noTone(BUZZER_PIN);
+    delay(100);
   }
 }
